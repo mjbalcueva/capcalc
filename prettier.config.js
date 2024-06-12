d6ace4ab64bbbe30e451a74def59b27ce1ab1f02
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-06-12 19:14:26 +0800

    chore: Update postcss.config.cjs, utils.ts, next.config.js, components.json, layout.tsx, prettier.config.js, and tsconfig.json files

--- prettier.config.js
+++ prettier.config.js
@@ -1,25 +1,25 @@
 /** @type {import('prettier').Config & import('@ianvs/prettier-plugin-sort-imports').PluginConfig} */
 const config = {
-  bracketSameLine: false,
-  importOrder: [
-    "^(next/(.*)$)|^(next$)|^(react/(.*)$)|^(react$)",
-    "<THIRD_PARTY_MODULES>",
-    "^@/(.*)$",
-    "^[./]",
-  ],
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
-  ],
-  printWidth: 80,
-  quoteProps: "consistent",
-  semi: true,
-  singleAttributePerLine: true,
-  singleQuote: false,
-  tabWidth: 2,
-  trailingComma: "all",
-  useTabs: true,
-};
+	bracketSameLine: false,
+	importOrder: [
+		'^(next/(.*)$)|^(next$)|^(react/(.*)$)|^(react$)',
+		'<THIRD_PARTY_MODULES>',
+		'^@/(.*)$',
+		'^[./]',
+	],
+	importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+	plugins: [
+		'@ianvs/prettier-plugin-sort-imports',
+		'prettier-plugin-tailwindcss',
+	],
+	printWidth: 80,
+	quoteProps: 'consistent',
+	semi: false,
+	singleAttributePerLine: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'all',
+	useTabs: true,
+}
 
-export default config;
+export default config
