0da2778162d0d5a6ae4ae7f6fa121bb9e16d05a3
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-06-12 19:28:37 +0800

    chore: Update ESLint and Prettier configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,13 @@
 const config = {
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		project: true,
+		project: true
 	},
 	plugins: ['@typescript-eslint'],
 	extends: [
 		'next/core-web-vitals',
 		'plugin:@typescript-eslint/recommended-type-checked',
-		'plugin:@typescript-eslint/stylistic-type-checked',
+		'plugin:@typescript-eslint/stylistic-type-checked'
 	],
 	rules: {
 		'@typescript-eslint/array-type': 'off',
@@ -17,24 +17,24 @@ const config = {
 			'warn',
 			{
 				prefer: 'type-imports',
-				fixStyle: 'inline-type-imports',
-			},
+				fixStyle: 'inline-type-imports'
+			}
 		],
 		'@typescript-eslint/no-unused-vars': [
 			'warn',
 			{
-				argsIgnorePattern: '^_',
-			},
+				argsIgnorePattern: '^_'
+			}
 		],
 		'@typescript-eslint/require-await': 'off',
 		'@typescript-eslint/no-misused-promises': [
 			'error',
 			{
 				checksVoidReturn: {
-					attributes: false,
-				},
-			},
-		],
-	},
+					attributes: false
+				}
+			}
+		]
+	}
 }
 module.exports = config
--- prettier.config.js
+++ prettier.config.js
@@ -5,21 +5,20 @@ const config = {
 		'^(next/(.*)$)|^(next$)|^(react/(.*)$)|^(react$)',
 		'<THIRD_PARTY_MODULES>',
 		'^@/(.*)$',
-		'^[./]',
+		'^[./]'
 	],
 	importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
 	plugins: [
 		'@ianvs/prettier-plugin-sort-imports',
-		'prettier-plugin-tailwindcss',
+		'prettier-plugin-tailwindcss'
 	],
 	printWidth: 80,
 	quoteProps: 'consistent',
 	semi: false,
-	singleAttributePerLine: true,
 	singleQuote: true,
 	tabWidth: 2,
-	trailingComma: 'all',
-	useTabs: true,
+	trailingComma: 'none',
+	useTabs: true
 }
 
 export default config
