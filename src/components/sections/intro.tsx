55e9642073ad4d015a212308f25b721cbe7c4a10
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-06-23 02:17:01 +0800

    refactor: Update layout and intro sections
    - Update layout.tsx to add the 'scroll-smooth' class to the html element
    - Remove unused import of Icons component in intro.tsx
    - Remove the logo component from the intro section
    
    Related to recent user commits:
    - Remove unused menuItems array in links.ts and update header component to use menuItem.content instead of menuItem.name
    - Update import of ActiveSectionProvider and ThemeProvider in layout.tsx
    - Update header styles for responsiveness
    - Optimize Meteors component with useMemo hook and generateMeteorStyle function
    - Update client-side imports in sections
    - Update background color in sections

--- src/components/sections/intro.tsx
+++ src/components/sections/intro.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { HeroSection } from '@/components/hero-section'
-import { Icons } from '@/components/icons'
 import { Meteors } from '@/components/ui/meteors'
 import { RetroGrid } from '@/components/ui/retro-grid'
 import { useSectionInView } from '@/lib/hooks'
@@ -18,7 +17,6 @@ const IntroSection = () => {
 			<HeroSection className="mt-14 flex flex-col items-center justify-center" />
 			<Meteors number={14} />
 			<RetroGrid className="mt-[30rem] max-h-[calc(100vh-30rem)]" />
-			<Icons.logo className="h-60 bg-red-400" />
 		</section>
 	)
 }
