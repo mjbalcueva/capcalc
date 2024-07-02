22c4fcc3a83c7ab93715f7f26466fcaf8d44778e
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-07-03 05:09:07 +0800

    Refactor input card to use react-hook-form for form handling

--- src/app/(calculators)/(non-built-up-columns)/nscp-2001-code-provisions/_components/input-card.tsx
+++ src/app/(calculators)/(non-built-up-columns)/nscp-2001-code-provisions/_components/input-card.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useEffect } from 'react'
 import { useFormContext } from 'react-hook-form'
 import { type z } from 'zod'
 
@@ -23,23 +22,14 @@ import {
 const InputCard = () => {
 	const {
 		control,
+		handleSubmit,
 		register,
 		reset,
-		trigger,
-		watch,
 		formState: { errors, isSubmitting }
 	} = useFormContext<z.infer<typeof nscp2001CodeProvisionsSchema>>()
 
-	useEffect(() => {
-		void trigger()
-	}, [trigger])
-
-	useEffect(() => {
-		const subscription = watch(() => {
-			void trigger()
-		})
-		return () => subscription.unsubscribe()
-	}, [watch, trigger])
+	const onSubmit = (data: z.infer<typeof nscp2001CodeProvisionsSchema>) =>
+		console.log(data)
 
 	return (
 		<Card>
@@ -48,107 +38,109 @@ const InputCard = () => {
 				<CardDescription>Input Description</CardDescription>
 			</CardHeader>
 
-			<CardContent className='px-6" flex flex-col space-y-4'>
-				<FormItem.Input
-					label="Yield Strength"
-					placeholder="MPa"
-					errorMessage={errors.Fy?.message}
-					{...register('Fy', {
-						valueAsNumber: true
-					})}
-				/>
+			<form onChange={handleSubmit(onSubmit)}>
+				<CardContent className='px-6" flex flex-col space-y-4'>
+					<FormItem.Input
+						label="Yield Strength"
+						placeholder="MPa"
+						errorMessage={errors.Fy?.message}
+						{...register('Fy', {
+							valueAsNumber: true
+						})}
+					/>
 
-				<FormItem.Input
-					label="Area"
-					placeholder="mm²"
-					errorMessage={errors.A?.message}
-					{...register('A', {
-						valueAsNumber: true
-					})}
-				/>
+					<FormItem.Input
+						label="Area"
+						placeholder="mm²"
+						errorMessage={errors.A?.message}
+						{...register('A', {
+							valueAsNumber: true
+						})}
+					/>
 
-				<FormItem.Input
-					label="Length of Column"
-					placeholder="mm"
-					errorMessage={errors.L?.message}
-					{...register('L', {
-						valueAsNumber: true
-					})}
-				>
-					<FormItem.Tooltip delayDuration={450}>
+					<FormItem.Input
+						label="Length of Column"
+						placeholder="mm"
+						errorMessage={errors.L?.message}
+						{...register('L', {
+							valueAsNumber: true
+						})}
+					>
+						<FormItem.Tooltip delayDuration={450}>
+							<FormItem.Controller
+								name="supportsMidspan"
+								control={control}
+								defaultValue={false}
+								render={({ field }) => (
+									<div className="absolute -top-[6px] right-0">
+										<FormItem.TooltipTrigger>
+											<FormItem.Switch
+												checked={field.value}
+												onCheckedChange={field.onChange}
+											/>
+										</FormItem.TooltipTrigger>
+										<FormItem.TooltipContent description="Enable Midspan Support" />
+									</div>
+								)}
+							/>
+						</FormItem.Tooltip>
+					</FormItem.Input>
+
+					<FormItem
+						label="Recomended or Theoretical"
+						errorMessage={errors.recommendedOrTheoretical?.message}
+					>
 						<FormItem.Controller
-							name="supportsMidspan"
+							name="recommendedOrTheoretical"
 							control={control}
-							defaultValue={false}
 							render={({ field }) => (
-								<div className="absolute -top-[6px] right-0">
-									<FormItem.TooltipTrigger>
-										<FormItem.Switch
-											checked={field.value}
-											onCheckedChange={field.onChange}
-										/>
-									</FormItem.TooltipTrigger>
-									<FormItem.TooltipContent description="Enable Midspan Support" />
-								</div>
+								<FormItem.Select
+									onValueChange={field.onChange}
+									defaultValue={field.value}
+									placeHolder="Select an option"
+									choices={recommendedOrTheoreticalChoices}
+								/>
 							)}
 						/>
-					</FormItem.Tooltip>
-				</FormItem.Input>
+					</FormItem>
 
-				<FormItem
-					label="Recomended or Theoretical"
-					errorMessage={errors.recommendedOrTheoretical?.message}
-				>
-					<FormItem.Controller
-						name="recommendedOrTheoretical"
-						control={control}
-						render={({ field }) => (
-							<FormItem.Select
-								onValueChange={field.onChange}
-								defaultValue={field.value}
-								placeHolder="Select an option"
-								choices={recommendedOrTheoreticalChoices}
-							/>
-						)}
+					<FormItem
+						label="Effective Length Factor"
+						errorMessage={errors.effectiveLengthFactor?.message}
+					>
+						<FormItem.Controller
+							name="effectiveLengthFactor"
+							control={control}
+							render={({ field }) => (
+								<FormItem.Select
+									onValueChange={field.onChange}
+									defaultValue={field.value}
+									placeHolder="Select an option"
+									choices={effectiveLengthFactorChoices}
+								/>
+							)}
+						/>
+					</FormItem>
+
+					<FormItem.Input
+						label="Moment of Inertia X"
+						placeholder="mm⁴"
+						errorMessage={errors.Ix?.message}
+						{...register('Ix', {
+							valueAsNumber: true
+						})}
 					/>
-				</FormItem>
 
-				<FormItem
-					label="Effective Length Factor"
-					errorMessage={errors.effectiveLengthFactor?.message}
-				>
-					<FormItem.Controller
-						name="effectiveLengthFactor"
-						control={control}
-						render={({ field }) => (
-							<FormItem.Select
-								onValueChange={field.onChange}
-								defaultValue={field.value}
-								placeHolder="Select an option"
-								choices={effectiveLengthFactorChoices}
-							/>
-						)}
+					<FormItem.Input
+						label="Moment of Inertia Y"
+						placeholder="mm⁴"
+						errorMessage={errors.Iy?.message}
+						{...register('Iy', {
+							valueAsNumber: true
+						})}
 					/>
-				</FormItem>
-
-				<FormItem.Input
-					label="Moment of Inertia X"
-					placeholder="mm⁴"
-					errorMessage={errors.Ix?.message}
-					{...register('Ix', {
-						valueAsNumber: true
-					})}
-				/>
-
-				<FormItem.Input
-					label="Moment of Inertia Y"
-					placeholder="mm⁴"
-					errorMessage={errors.Iy?.message}
-					{...register('Iy', {
-						valueAsNumber: true
-					})}
-				/>
-			</CardContent>
+				</CardContent>
+			</form>
 
 			<CardFooter className="flex flex-col">
 				<Button
