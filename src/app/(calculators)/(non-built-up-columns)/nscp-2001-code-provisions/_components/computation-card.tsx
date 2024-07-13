0fb3ea2041838068e82fdf35f162e32709a27282
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-07-13 23:27:21 +0800

    refactor: Update computation card to handle infinite values gracefully

--- src/app/(calculators)/(non-built-up-columns)/nscp-2001-code-provisions/_components/computation-card.tsx
+++ src/app/(calculators)/(non-built-up-columns)/nscp-2001-code-provisions/_components/computation-card.tsx
@@ -17,27 +17,27 @@ const ComputationCard = () => {
 			<CardContent className="grid gap-4 text-center sm:grid-cols-2 sm:gap-8 sm:text-start">
 				<div className="flex flex-col space-y-4">
 					<FormOutput label="Radius of Gyration" symbol="Rx">
-						{values.Rx == Infinity ? 0 : values.Rx || 0}
+						{!isFinite(values.Rx) ? 0 : values.Rx || 0}
 					</FormOutput>
 					<FormOutput label="Radius of Gyration" symbol="Ry">
-						{values.Ry == Infinity ? 0 : values.Ry || 0}
+						{!isFinite(values.Ry) ? 0 : values.Ry || 0}
 					</FormOutput>
 					<FormOutput label="Minimum Radius" symbol="Rmin">
-						{values.rMin == Infinity ? 0 : values.rMin || 0}
+						{!isFinite(values.rMin) ? 0 : values.rMin || 0}
 					</FormOutput>
 					<FormOutput label="Critical Stress" symbol="Cc">
-						{values.Cc == Infinity ? 0 : values.Cc || 0}
+						{!isFinite(values.Cc) ? 0 : values.Cc || 0}
 					</FormOutput>
 				</div>
 				<div className="flex flex-col space-y-4">
 					<FormOutput label="Slenderness Ratio" symbol="SRx">
-						{values.SRx == Infinity ? 0 : values.SRx || 0}
+						{!isFinite(values.SRx) ? 0 : values.SRx || 0}
 					</FormOutput>
 					<FormOutput label="Slenderness Ratio" symbol="SRy">
-						{values.SRy == Infinity ? 0 : values.SRy || 0}
+						{!isFinite(values.SRy) ? 0 : values.SRy || 0}
 					</FormOutput>
 					<FormOutput label="Maximum" symbol="SRmax">
-						{values.SRmax == Infinity ? 0 : values.SRmax || 0}
+						{!isFinite(values.SRmax) ? 0 : values.SRmax || 0}
 					</FormOutput>
 					<FormOutput label="Column Type" symbol="">
 						{values.ColumnType || 'None'}
