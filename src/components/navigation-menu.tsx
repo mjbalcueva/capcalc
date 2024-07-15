355c090bfbf8a43c999c7766e1388a3f8bf35228
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-07-15 21:11:33 +0800

    refactor: Extract MenuItem component in NavigationMenu

--- src/components/navigation-menu.tsx
+++ src/components/navigation-menu.tsx
@@ -21,42 +21,74 @@ const NavigationMenu = () => {
 
 			<nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
 				<ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-screen sm:flex-nowrap sm:gap-5">
-					{menuItems.map((menuItem) => (
-						<motion.li
-							className="relative flex h-3/4 items-center justify-center"
-							key={menuItem.hash}
-							initial={{ y: -100, opacity: 0 }}
-							animate={{ y: 0, opacity: 1 }}
-						>
-							<Link
-								className={cn(
-									'flex w-full items-center justify-center px-3 py-3 text-center transition hover:text-gray-950 dark:hover:text-gray-300',
-									`${menuItem.hash === activeSection ? 'text-gray-950 dark:text-gray-200' : 'text-gray-500 dark:text-gray-500'}`
-								)}
-								href={`/${menuItem.hash}`}
-								onClick={() => {
-									setActiveSection(menuItem.hash)
-									setTimeOfLastClick(Date.now())
-								}}
-							>
-								{menuItem.content}
-								{menuItem.hash === activeSection && (
-									<motion.span
-										className="absolute inset-0 -z-10 rounded-full bg-gray-200/55 dark:bg-gray-700/30"
-										layoutId="activeSection"
-										transition={{
-											type: 'spring',
-											stiffness: 380,
-											damping: 30
-										}}
-									></motion.span>
-								)}
-							</Link>
-						</motion.li>
-					))}
+					<MenuItem
+						menuItem={menuItems[0]}
+						activeSection={activeSection}
+						setActiveSection={setActiveSection}
+						setTimeOfLastClick={setTimeOfLastClick}
+					/>
+					<MenuItem
+						menuItem={menuItems[1] as { content: string; hash: string }}
+						activeSection={activeSection}
+						setActiveSection={setActiveSection}
+						setTimeOfLastClick={setTimeOfLastClick}
+					/>
+					<MenuItem
+						menuItem={menuItems[3] as { content: string; hash: string }}
+						activeSection={activeSection}
+						setActiveSection={setActiveSection}
+						setTimeOfLastClick={setTimeOfLastClick}
+					/>
+					<MenuItem
+						menuItem={menuItems[2] as { content: string; hash: string }}
+						activeSection={activeSection}
+						setActiveSection={setActiveSection}
+						setTimeOfLastClick={setTimeOfLastClick}
+					/>
 				</ul>
 			</nav>
 		</header>
+	)
+}
+
+const MenuItem: React.FC<{
+	menuItem: (typeof menuItems)[number]
+	activeSection: string
+	setActiveSection: (section: string) => void
+	setTimeOfLastClick: (time: number) => void
+}> = ({ menuItem, activeSection, setActiveSection, setTimeOfLastClick }) => {
+	return (
+		<motion.li
+			className="relative flex h-3/4 items-center justify-center"
+			key={menuItem.hash}
+			initial={{ y: -100, opacity: 0 }}
+			animate={{ y: 0, opacity: 1 }}
+		>
+			<Link
+				className={cn(
+					'flex w-full items-center justify-center px-3 py-3 text-center transition hover:text-gray-950 dark:hover:text-gray-300',
+					`${menuItem.hash === activeSection ? 'text-gray-950 dark:text-gray-200' : 'text-gray-500 dark:text-gray-500'}`
+				)}
+				href={`/${menuItem.hash}`}
+				onClick={() => {
+					setActiveSection(menuItem.hash)
+					setTimeOfLastClick(Date.now())
+				}}
+			>
+				{menuItem.content}
+				{menuItem.hash === activeSection && (
+					<motion.span
+						className="absolute inset-0 -z-10 rounded-full bg-gray-200/55 dark:bg-gray-700/30"
+						layoutId="activeSection"
+						transition={{
+							type: 'spring',
+							stiffness: 380,
+							damping: 30
+						}}
+					></motion.span>
+				)}
+			</Link>
+		</motion.li>
 	)
 }
 
