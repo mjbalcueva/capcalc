d6efc87625ac64ac09ecbfcf9bbd447250f2d928
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-09 17:22:05 +0800

    eccentric bold layout

--- src/app/(calculators)/(bolted-connections)/eccentric-bolted-connection/_components/computation-card.tsx
+++ src/app/(calculators)/(bolted-connections)/eccentric-bolted-connection/_components/computation-card.tsx
@@ -1,19 +1,92 @@
 'use client'
 
-// import { useAtom } from 'jotai'
+import { useAtom } from 'jotai'
 
-// import { calculatedAtoms } from '@/atoms/eccentric-bolted-connection'
-import { useFormContext } from 'react-hook-form'
+import { calculatedAtoms } from '@/atoms/eccentric-bolted-connection'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-
-import { type inputType } from '@/lib/schemas/eccentric-bolted-connection'
-
-// import { FormOutput } from '@/components/ui/form'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 
 const ComputationCard = () => {
-	// const [values] = useAtom(calculatedAtoms)
-	const form = useFormContext<inputType>()
+	const [values] = useAtom(calculatedAtoms)
+
+	// const tableData = [
+	// 	{ label: 'Eccentricity along x-axis', variable: 'ex', value: values.ex, unit: 'mm' },
+	// 	{ label: 'Eccentricity along y-axis', variable: 'ey', value: values.ey, unit: 'mm' },
+	// 	{ label: 'Torque', variable: 'T', value: values.T, unit: 'kN-mm' },
+	// 	{ label: 'Polar Moment of Inertia', variable: 'J', value: values.J, unit: 'mm^4' },
+	// 	{ label: 'Load Due to Torque (x-direction)', variable: 'PTx1', value: values.PTx.PTx1, unit: 'kN' },
+	// 	{ label: 'Load Due to Torque (x-direction)', variable: 'PTx2', value: values.PTx.PTx2, unit: 'kN' },
+	// 	{ label: 'Load Due to Torque (x-direction)', variable: 'PTx3', value: values.PTx.PTx3, unit: 'kN' },
+	// 	{ label: 'Load Due to Torque (x-direction)', variable: 'PTx4', value: values.PTx.PTx4, unit: 'kN' },
+	// 	{ label: 'Load Due to Torque (y-direction)', variable: 'PTy1', value: values.PTy.PTy1, unit: 'kN' },
+	// 	{ label: 'Load Due to Torque (y-direction)', variable: 'PTy2', value: values.PTy.PTy2, unit: 'kN' },
+	// 	{ label: 'Load Due to Torque (y-direction)', variable: 'PTy3', value: values.PTy.PTy3, unit: 'kN' },
+	// 	{ label: 'Load Due to Torque (y-direction)', variable: 'PTy4', value: values.PTy.PTy4, unit: 'kN' },
+	// 	{ label: 'Direct Load (x-direction)', variable: 'PDx', value: values.PDx, unit: 'kN' },
+	// 	{ label: 'Direct Load (y-direction)', variable: 'PDy', value: values.PDy, unit: 'kN' },
+	// 	{ label: 'Load Along x-axis', variable: 'Px1', value: values.Px.Px1, unit: 'kN' },
+	// 	{ label: 'Load Along x-axis', variable: 'Px2', value: values.Px.Px2, unit: 'kN' },
+	// 	{ label: 'Load Along x-axis', variable: 'Px3', value: values.Px.Px3, unit: 'kN' },
+	// 	{ label: 'Load Along x-axis', variable: 'Px4', value: values.Px.Px4, unit: 'kN' },
+	// 	{ label: 'Load Along y-axis', variable: 'Py1', value: values.Py.Py1, unit: 'kN' },
+	// 	{ label: 'Load Along y-axis', variable: 'Py2', value: values.Py.Py2, unit: 'kN' },
+	// 	{ label: 'Load Along y-axis', variable: 'Py3', value: values.Py.Py3, unit: 'kN' },
+	// 	{ label: 'Load Along y-axis', variable: 'Py4', value: values.Py.Py4, unit: 'kN' },
+	// 	{ label: 'Total Load', variable: 'P1', value: values.P.P1, unit: 'kN' },
+	// 	{ label: 'Total Load', variable: 'P2', value: values.P.P2, unit: 'kN' },
+	// 	{ label: 'Total Load', variable: 'P3', value: values.P.P3, unit: 'kN' },
+	// 	{ label: 'Total Load', variable: 'P4', value: values.P.P4, unit: 'kN' }
+	// ]
+
+	const eccentricity = [
+		{ label: 'Eccentricity along x-axis', variable: 'ex', value: values.ex, unit: 'mm' },
+		{ label: 'Eccentricity along y-axis', variable: 'ey', value: values.ey, unit: 'mm' }
+	]
+
+	const torque = [{ label: 'Torque', variable: 'T', value: values.T, unit: 'kN-mm' }]
+
+	const polarMomentOfInertia = [{ label: 'Polar Moment of Inertia', variable: 'J', value: values.J, unit: 'mm^4' }]
+
+	const loadDueToTorque = [
+		{ label: 'Load Due to Torque (x-direction)', variable: '1', value: values.PTx.PTx1, unit: 'kN' },
+		{ label: 'Load Due to Torque (x-direction)', variable: '2', value: values.PTx.PTx2, unit: 'kN' },
+		{ label: 'Load Due to Torque (x-direction)', variable: '3', value: values.PTx.PTx3, unit: 'kN' },
+		{ label: 'Load Due to Torque (x-direction)', variable: '4', value: values.PTx.PTx4, unit: 'kN' }
+	]
+
+	const loadDueToTorqueY = [
+		{ label: 'Load Due to Torque (y-direction)', variable: '1', value: values.PTy.PTy1, unit: 'kN' },
+		{ label: 'Load Due to Torque (y-direction)', variable: '2', value: values.PTy.PTy2, unit: 'kN' },
+		{ label: 'Load Due to Torque (y-direction)', variable: '3', value: values.PTy.PTy3, unit: 'kN' },
+		{ label: 'Load Due to Torque (y-direction)', variable: '4', value: values.PTy.PTy4, unit: 'kN' }
+	]
+
+	const directLoad = [
+		{ label: 'Direct Load (x-direction)', variable: 'All bolts', value: values.PDx, unit: 'kN' },
+		{ label: 'Direct Load (y-direction)', variable: 'All bolts', value: values.PDy, unit: 'kN' }
+	]
+
+	const loadAlongX = [
+		{ label: 'Load Along x-axis', variable: '1', value: values.Px.Px1, unit: 'kN' },
+		{ label: 'Load Along x-axis', variable: '2', value: values.Px.Px2, unit: 'kN' },
+		{ label: 'Load Along x-axis', variable: '3', value: values.Px.Px3, unit: 'kN' },
+		{ label: 'Load Along x-axis', variable: '4', value: values.Px.Px4, unit: 'kN' }
+	]
+
+	const loadAlongY = [
+		{ label: 'Load Along y-axis', variable: '1', value: values.Py.Py1, unit: 'kN' },
+		{ label: 'Load Along y-axis', variable: '2', value: values.Py.Py2, unit: 'kN' },
+		{ label: 'Load Along y-axis', variable: '3', value: values.Py.Py3, unit: 'kN' },
+		{ label: 'Load Along y-axis', variable: '4', value: values.Py.Py4, unit: 'kN' }
+	]
+
+	const totalLoad = [
+		{ label: 'Total Load', variable: '1', value: values.P.P1, unit: 'kN' },
+		{ label: 'Total Load', variable: '2', value: values.P.P2, unit: 'kN' },
+		{ label: 'Total Load', variable: '3', value: values.P.P3, unit: 'kN' },
+		{ label: 'Total Load', variable: '4', value: values.P.P4, unit: 'kN' }
+	]
 
 	return (
 		<Card>
@@ -21,36 +94,50 @@ const ComputationCard = () => {
 				<CardTitle>Computation Variables</CardTitle>
 			</CardHeader>
 
-			<CardContent>
-				<pre>{JSON.stringify(form.getValues(), null, 2)}</pre>
+			<CardContent className="space-y-4">
+				<ComputationTable data={eccentricity} />
+				<ComputationTable data={torque} />
+				<ComputationTable data={polarMomentOfInertia} />
+				<ComputationTable isBolt data={loadDueToTorque} />
+				<ComputationTable isBolt data={loadDueToTorqueY} />
+				<ComputationTable isBolt data={directLoad} />
+				<ComputationTable isBolt data={loadAlongX} />
+				<ComputationTable isBolt data={loadAlongY} />
+				<ComputationTable isBolt data={totalLoad} />
 			</CardContent>
-
-			{/* <CardContent className="grid gap-4 text-center sm:grid-cols-2 sm:gap-8 sm:text-start">
-				{typeOfTensileCapacityChoices === 'getCapacity' && (
-					<>
-						<FormOutput label="Gross Area" symbol="Ag">
-							{!isFinite(values.Ag) ? 0 : values.Ag || 0}
-						</FormOutput>
-
-						<FormOutput label="Net Area" symbol="An">
-							{!isFinite(values.An) ? 0 : values.An || 0}
-						</FormOutput>
-					</>
-				)}
-
-				{typeOfTensileCapacityChoices === 'getP' && (
-					<>
-						<FormOutput label="Area of Weld Parallel to the load" symbol="Ag">
-							{!isFinite(values.Av) ? 0 : values.Av || 0}
-						</FormOutput>
-
-						<FormOutput label="Area of Weld Transverse to the load" symbol="An">
-							{!isFinite(values.At) ? 0 : values.At || 0}
-						</FormOutput>
-					</>
-				)}
-			</CardContent> */}
 		</Card>
+	)
+}
+
+const ComputationTable = ({
+	isBolt,
+	data
+}: {
+	isBolt?: boolean
+	data: { label: string; variable: string; value: number; unit: string }[]
+}) => {
+	return (
+		<Table className="rounded-xl border border-border">
+			<TableHeader className="bg-muted text-muted-foreground">
+				<TableRow>
+					<TableHead>Label</TableHead>
+					<TableHead>{isBolt ? 'Bolt' : 'Variable'}</TableHead>
+					<TableHead className="w-[130px]">Value</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				{data.map((item, index) => (
+					<TableRow key={index} className="font-medium">
+						<TableCell className="font-medium">{item.label}</TableCell>
+						<TableCell className="w-1/4 font-normal">{item.variable}</TableCell>
+						<TableCell className="flex w-[130px] justify-end text-right font-normal">
+							<span>{item.value}</span>
+							<span className="flex-grow text-muted-foreground">{item.unit}</span>
+						</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
+		</Table>
 	)
 }
 
--- src/app/(calculators)/(bolted-connections)/eccentric-bolted-connection/_components/result-card.tsx
+++ src/app/(calculators)/(bolted-connections)/eccentric-bolted-connection/_components/result-card.tsx
@@ -16,35 +16,27 @@ const ResultCard = () => {
 				<CardTitle>Results</CardTitle>
 			</CardHeader>
 
-			<CardContent>
-				<pre>{JSON.stringify(values, null, 2)}</pre>
+			<CardContent className="grid gap-4 text-center sm:grid-cols-2 sm:gap-8 sm:text-start">
+				<div className="flex flex-col gap-4">
+					<FormOutput label="Shearing Stress" symbol="fv 1">
+						{!isFinite(values.fv.fv1) ? 0 : values.fv.fv1 || 0}
+					</FormOutput>
+
+					<FormOutput label="Shearing Stress" symbol="fv 2">
+						{!isFinite(values.fv.fv2) ? 0 : values.fv.fv2 || 0}
+					</FormOutput>
+				</div>
+
+				<div className="flex flex-col gap-4">
+					<FormOutput label="Shearing Stress" symbol="fv 3">
+						{!isFinite(values.fv.fv3) ? 0 : values.fv.fv3 || 0}
+					</FormOutput>
+
+					<FormOutput label="Shearing Stress" symbol="fv 4">
+						{!isFinite(values.fv.fv4) ? 0 : values.fv.fv4 || 0}
+					</FormOutput>
+				</div>
 			</CardContent>
-
-			{/* <CardContent className="grid gap-4 text-center sm:grid-cols-2 sm:gap-8 sm:text-start">
-				{typeOfTensileCapacityChoices === 'getCapacity' && (
-					<div className="flex flex-col gap-4">
-						<FormOutput label="P Based on Gross Area" symbol="P1">
-							{!isFinite(values.P1) ? 0 : values.P1 || 0}
-						</FormOutput>
-
-						<FormOutput label="P Based on Net Area" symbol="P2">
-							{!isFinite(values.P2) ? 0 : values.P2 || 0}
-						</FormOutput>
-					</div>
-				)}
-
-				{typeOfTensileCapacityChoices === 'getP' && (
-					<FormOutput label="P Based on Block Shear" symbol="An">
-						{!isFinite(values.P3) ? 0 : values.P3 || 0}
-					</FormOutput>
-				)}
-
-				{typeOfTensileCapacityChoices === 'getCapacity' && (
-					<FormOutput label="Capacity" symbol="P">
-						{!isFinite(values.smallestP) ? 0 : values.smallestP || 0}
-					</FormOutput>
-				)} 
-			</CardContent> */}
 		</Card>
 	)
 }
